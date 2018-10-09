eb9436f33d11535af87045ee3c64adad4610a8f2
derco0n <dennis_marx@web.de>
2018-10-09 11:38:09 +0200

    Installer... :/

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import setup
 
+entry_points = {
+    "console_scripts": [
+        "packman = PackMan.__init__"
+    ]
+}
+
 setup(
     name='PackMan',
     version='0.13',
-    packages=[''],
+    packages=['PackMan'],
     url='https://github.com/derco0n/PackMan',
+    entry_points=entry_points,
+    install_requires=[
+        "mysqlclient==1.3.7",
+        "RPi.GPIO==0.6.3",
+    ],
     license='GPL v3',
     author='D. Marx',
     author_email='',
